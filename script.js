b05f94166aa9dc68b6f2d67be15afccd46dfd878
anjalii13 <anjuu1304@gmail.com>
2026-04-08 13:11:11 +0530

    second

--- script.js
+++ script.js
@@ -10,7 +10,21 @@ const input="Hello World From Wisdom Sprouts IT Training Hub";
 const output=reverse(input);
 console.log(output);
 
+
+
 // Task-2
+function para(text){
+    return text
+    .toLowerCase()
+    .trim()
+    .replace(/[@!#$%^&*()_+={}\[\]|\\:;'"<>,.?/~`-]/g, '')
+    ;
+
+
+}
+const text1 ="Hii@@ my name is anjali and i am a student-- of wisdom sprouts it training hub!!";
+console.log(para(text1));
+
 
 // Task-3
 function filterAdults(users) {
@@ -23,8 +37,9 @@ const users = [
 ];
 console.log(filterAdults(users)); 
 
-// Task-4
 
+
+// Task-4
 function sortProductsByPrice(products) {
     return products.sort((a, b) => a.price - b.price);
 }
@@ -37,8 +52,9 @@ const products = [
 
 console.log(sortProductsByPrice(products));
 
-// Task-5
 
+
+// Task-5
 function generateSlug(text) {
     return text
         .toLowerCase()
@@ -51,8 +67,9 @@ const text="Hello World!!!From Wisdom Sprouts!!IT-Training Hub$%";
 console.log(generateSlug(text));
 
 
-// Task-6
 
+
+// Task-6
 function groupWordsByLength(words) {
     return words.reduce((result, word) => {
         const length = word.length;
@@ -65,3 +82,40 @@ function groupWordsByLength(words) {
 }
 const words = ['apple', 'cat', 'dog', 'elephant', 'bat', 'programming','book'];
 console.log(groupWordsByLength(words));
+
+
+// Task-9
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function taskA() {
+    const randomDelay = Math.random() * 500 + 500; 
+    await delay(randomDelay);
+    console.log('Task A completed');
+}
+
+async function taskB() {
+    const randomDelay = Math.random() * 500 + 500; 
+    await delay(randomDelay);
+    console.log('Task B completed');
+}
+
+async function taskC() {
+    const randomDelay = Math.random() * 500 + 500; 
+    await delay(randomDelay);
+    console.log('Task C completed');
+}
+
+async function runTasks() {
+    await taskA();
+    await taskB();
+    await taskC();
+    console.log('All tasks completed');
+}
+
+runTasks();
+
+
+
