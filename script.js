33b2ba3d76c253742a7493652b5329d64adddd0a
anjalii13 <anjuu1304@gmail.com>
2026-04-08 12:36:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+// Task-1
+function reverse(sentence){
+    return sentence
+    .split(" ")
+    .map(word=>word.split("").reverse().join(""))
+    .join("");
+
+}
+const input="Hello World From Wisdom Sprouts IT Training Hub";
+const output=reverse(input);
+console.log(output);
+
+// Task-2
+
+// Task-3
+function filterAdults(users) {
+    return users.filter(users => users.age > 18);
+}
+const users = [
+    { name: 'Alice', age: 25 },
+    { name: 'Bob', age: 17 },
+    { name: 'Charlie', age: 30 }
+];
+console.log(filterAdults(users)); 
+
+// Task-4
+
+function sortProductsByPrice(products) {
+    return products.sort((a, b) => a.price - b.price);
+}
+
+const products = [
+    { name: 'Apple', price: 2 },
+    { name: 'Banana', price: 1 },
+    { name: 'Cherry', price: 3 }
+];
+
+console.log(sortProductsByPrice(products));
+
+// Task-5
+
+function generateSlug(text) {
+    return text
+        .toLowerCase()
+        .trim()
+        .replace(/[^\w\s-]/g, '')
+        .replace(/\s+/g, '-');
+}
+
+const text="Hello World!!!From Wisdom Sprouts!!IT-Training Hub$%";
+console.log(generateSlug(text));
+
+
+// Task-6
+
+function groupWordsByLength(words) {
+    return words.reduce((result, word) => {
+        const length = word.length;
+        if (!result[length]) {
+            result[length] = [];
+     }
+        result[length].push(word);
+        return result;
+    }, {});
+}
+const words = ['apple', 'cat', 'dog', 'elephant', 'bat', 'programming','book'];
+console.log(groupWordsByLength(words));
